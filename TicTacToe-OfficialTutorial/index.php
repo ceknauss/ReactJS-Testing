3a95b3dfc5cfe1e51b9745b2379eb40c0dcdce0f
Corwin K <ceknauss@gmail.com>
2019-03-22 16:27:37 -0500

    Official ReactJS Tutorial - Initial Commit
    TicTacToe tutorial from the Official ReactJS 'Take the Tutorial' guide. Initial commit with basic building blocks.

---
+++ TicTacToe-OfficialTutorial/index.php
@@ -0,0 +1,30 @@
+<html>
+
+<head>
+	<!-- Title -->
+	<title>ReactJS Testing </title>
+
+	<!-- ReactJS Script Includes -->
+
+		<!-- DEVELOPMENT SCRIPTS
+		<script src="https://unpkg.com/react@16/umd/react.development.js"></script>
+		<script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js"></script>
+		-->
+		<!-- Production Scripts -->
+		<script src="https://unpkg.com/react@16/umd/react.production.min.js"></script>
+		<script src="https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"></script>
+		<script src="https://unpkg.com/babel-standalone@6.15.0/babel.min.js"></script>
+
+	<!-- ReactJS Application -->
+	<link rel="stylesheet" type="text/css" href="css/TicTacToe.css">
+	<script src="app/view_index.js" type="text/babel"></script>
+</head>
+
+<body>
+
+	<!-- ReactJS Container -->
+	<div id="ReactJS_Root"></div>
+
+</body>
+
+</html>
